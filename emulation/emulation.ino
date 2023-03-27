2ff0678398a4b4d50787ced4ba946e727dcc0ee6
Kirk Roerig <mr.possoms@gmail.com>
2023-03-27 12:44:55 -0600

    logging

--- emulation/emulation.ino
+++ emulation/emulation.ino
@@ -20,6 +20,8 @@ void handle_receive(int bytes_received)
   if (bytes_received == 1)
   { // we are being addressed first
     CTX.sel_reg = Wire.read();
+
+    Serial.print("Selected reg: "); Serial.println(CTX.sel_reg);
   }
   else
   {
@@ -28,11 +30,19 @@ void handle_receive(int bytes_received)
     {
       r_ptr += Wire.readBytes(r_ptr, bytes_received);
     }
+
+    Serial.print("Received: ");
+    for (unsigned i = 0; i < bytes_received; i++)
+    {
+      Serial.print(CTX.rx_buf[i], HEX); Serial.print(" ");
+    } Serial.println();
   }
 }
 
 void setup()
 {
+  Serial.begin(115200);
+
   Wire.onReceive(handle_receive);
   Wire.onRequest(handle_request);
   Wire.begin(ZED_FP9_ADDR);
