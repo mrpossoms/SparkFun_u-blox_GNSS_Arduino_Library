8776c30f23c297cdb602c6180ab3438707d51093
Kirk Roerig <mr.possoms@gmail.com>
2023-03-27 21:15:45 -0600

    Cleanup

--- emulation/emulation.ino
+++ emulation/emulation.ino
@@ -5,10 +5,6 @@
 
 static emulation::ZED_FP9 rtk;
 
-// struct {
-//   uint8_t sel_reg;
-//   uint8_t rx_buf[512];
-// } CTX;
 
 void handle_request()
 {
@@ -24,28 +20,6 @@ void handle_receive(int bytes_received)
   Serial.print("Received: "); Serial.println(bytes_received);
 
   rtk.service_receive(Wire, bytes_received);
-/*
-  if (bytes_received == 1)
-  { // we are being addressed first
-    CTX.sel_reg = Wire.read();
-
-    Serial.print("Selected reg: "); Serial.println(CTX.sel_reg, HEX);
-  }
-  else
-  {
-    uint8_t* s_ptr = CTX.rx_buf;
-    uint8_t* r_ptr = CTX.rx_buf;
-    while ((r_ptr - s_ptr) < bytes_received)
-    {
-      r_ptr += Wire.readBytes(r_ptr, bytes_received);
-    }
-
-    for (unsigned i = 0; i < bytes_received; i++)
-    {
-      Serial.print(CTX.rx_buf[i], HEX); Serial.print(" ");
-    } Serial.println();
-  }
-*/
 }
 
 void setup()
